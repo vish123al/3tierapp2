95e9dc74277d6f2a90d1ed43a8c479d917bac5b0
vish123al <vishaldenge1@gmail.com>
2017-09-29 15:58:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,11 @@ $db_password = '';
 $db_name = 'myapp';
 $db_host = '10.0.1.87';
 $db_port = 3306;
-$mysqli = new mysqli('10.0.1.87','root','password','myapp',9880);
+//$mysqli = new mysqli('10.0.1.87','root','password','myapp',9880);
+$mysqli = new mysqli("127.0.0.1","root","password","myapp",3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
 ?>
