5c298b927d9ea11799d3e1c280fa21aff3e8ade6
vish123al <vishaldenge1@gmail.com>
2017-09-29 13:17:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
 $db_username = 'root';
-$db_password = '';
-$db_name = 'webapp2';
-$db_host = 'localhost';
+$db_password = 'password';
+$db_name = 'webapp';
+$db_host = '10.0.1.81:7170';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
