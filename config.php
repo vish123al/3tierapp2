aaba8510225fcc95d62ea061928f1aea05cb634c
vish123al <vishaldenge1@gmail.com>
2017-09-29 17:16:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_name = 'myapp';
 $db_host = '10.0.1.87';
 $db_port = 3306;
 //$mysqli = new mysqli('10.0.1.87','root','password','myapp',9880);
-$mysqli = new mysqli("10.0.1.87","root","root","myapp",3306);
+$mysqli = new mysqli("mysql.marathon.messos","root","root","myapp",3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
