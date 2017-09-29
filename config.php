87ca89eefef6742055445d6ec93afcc18f4f3487
vish123al <vishaldenge1@gmail.com>
2017-09-29 15:49:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ $db_password = '';
 $db_name = 'myapp';
 $db_host = '10.0.1.87';
 $db_port = 3306;
-$mysqli = new mysqli('10.0.1.87','root','password','myapp',3306);
+$mysqli = new mysqli('10.0.1.87','root','password','myapp',9880);
 ?>
